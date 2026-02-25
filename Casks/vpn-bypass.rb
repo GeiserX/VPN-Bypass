7c2882df6c1b82f0bd21ad2564e98827a126404d
GeiserX <9169332+GeiserX@users.noreply.github.com>
2026-02-25 21:07:45 +0100

    chore: update Cask SHA256 for v1.8.2 release

--- Casks/vpn-bypass.rb
+++ Casks/vpn-bypass.rb
@@ -3,8 +3,8 @@
 # Or if using local tap: brew install --cask --no-quarantine ./Casks/vpn-bypass.rb
 
 cask "vpn-bypass" do
-  version "1.8.1"
-  sha256 "489867e2c76802321a5f28a32dcf18366c96d94585035626eb5dbbefb77a33eb"
+  version "1.8.2"
+  sha256 "e3d2b80be266c1a703c0b00172fa4ad843225898a82662381061b6781d656702"
 
   url "https://github.com/GeiserX/VPN-Bypass/releases/download/v#{version}/VPN-Bypass-#{version}.dmg"
   name "VPN Bypass"
